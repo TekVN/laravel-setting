cc2797de3c3dcfcd33e2bb89be901e3d67747d18
DNT <ducconit@gmail.com>
2023-05-16 20:31:21 +0700

    docs: add phpdoc for Facade.php

--- src/Facade.php
+++ src/Facade.php
@@ -2,8 +2,29 @@
 
 namespace DNT\Setting;
 
+use DNT\Setting\Contracts\Setting as SettingsContract;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade as BaseFacade;
 
+/**
+ * This class serves as the facade for accessing the Setting library in a Laravel application.
+ * It provides a static interface for interacting with the Setting instance without the need
+ * for manual dependency injection.
+ *
+ * @method static mixed get(string $key, mixed $default = null)
+ * @method static static set(string|array $key, mixed $value = null)
+ * @method static void forget(string|array $key)
+ * @method static array save()
+ * @method static bool has(string $key)
+ * @method static array load()
+ * @method static array refresh()
+ * @method static array all()
+ * @method static Collection collection()
+ * @method static array changed()
+ * @method static bool isChanged()
+ *
+ * @see SettingsContract
+ */
 class Facade extends BaseFacade
 {
     /**
@@ -11,6 +32,6 @@ class Facade extends BaseFacade
      */
     public static function getFacadeAccessor(): string
     {
-        return 'setting';
+        return SettingsContract::class;
     }
 }
